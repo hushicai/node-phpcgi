f75c86c972f5ecc25cc5d53da32d67413ffba1f1
hushicai <bluthcy@gmail.com>
2014-11-05 20:16:20 +0800

    add mock

--- mock/app.js
+++ mock/app.js
@@ -1,17 +1,21 @@
 var serveStatic = require('serve-static');
 var serveIndex = require('serve-index');
 var bodyParser = require('body-parser');
-var phpcgi = require('./main');
+var phpcgi = require('../main');
 
 var express = require('express');
 var app = express();
 
 app.set('port', 8000);
 
+var documentRoot = require('path').resolve(__dirname, '..');
+
+console.log(documentRoot);
+
 app.use(bodyParser.raw());
-app.use(phpcgi({documentRoot: __dirname}));
-app.use(serveIndex(__dirname));
-app.use(serveStatic(__dirname));
+app.use(phpcgi({documentRoot: documentRoot}));
+app.use(serveIndex(documentRoot));
+app.use(serveStatic(documentRoot));
 
 app.listen(
     app.get('port'),

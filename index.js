f7e52eb8060a0a90a2c186ee1130dc6b1e253c3b
hushicai <bluthcy@gmail.com>
2014-11-24 10:09:42 +0800

    add edp entry

--- index.js
+++ index.js
@@ -22,31 +22,4 @@ function fn(options) {
     };
 };
 
-// edp php handler
-fn.phpHandlerForEdp = function(options) {
-    options = options || {};
-    var handler = options.handler;
-    return function(context) {
-        return phpcgi({
-            start: function() {
-                context.stop();
-            },
-            documentRoot: context.documentRoot,
-            handler: handler,
-            req: context.request,
-            res: context.response,
-            end: function(e) {
-                context.status = e.statusCode;
-                context.content = e.body;
-                var headers = e.headers || {};
-                for (var key in headers) {
-                    context.header[key] = headers[key];
-                }
-                context.phpOk = true;
-                context.start();
-            }
-        });
-    };
-};
-
 exports = module.exports = fn;
